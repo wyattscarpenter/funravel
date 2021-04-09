9a056f6885dfb216160b4a64834a68a8180d7acb
wyattscarpenter <wyattscarpenter@gmail.com>
2021-04-09 14:01:59 +0100

    clean up intermediate code a bit

--- funravel.py
+++ funravel.py
@@ -3,24 +3,20 @@
 row_hint = ""
 col_hint = ""
 
-#help(ipywidgets.ToggleButtons)
-
 hints = ["This is "+ x for x in ['csv', 'json', 'xml', 'xls', 'wikipedia table']]+["Separator "+ x for x in ['⏎⏎', '⏎', 'tab', 'space','|']] + ["Container "+ x for x in ['{}','[]','()', '""', "''"]]
 
 #callbacks for hint buttons
 def set_row_hint(b):
   global row_hint
   row_hint = b.new
-  #never could get this to print debug messages right...
 def set_col_hint(b):
   global col_hint
   col_hint = b.new
-  #never could get this to print debug messages right...
 
 #ui
 try:
   import ipywidgets
-  #could factor this out but it's not obvious that would be clearer
+  #TODO: set default values
   b = ipywidgets.ToggleButtons(options=hints,description="row")
   b.observe(set_row_hint, 'value') #I guess this is how you're supposed to do it.
   display(b)
@@ -28,15 +24,15 @@ try:
   b.observe(set_col_hint, 'value')
   display(b)
 except Exception as e:
-  if str(e) == "No module named 'ipywidgets'":
-    print("You need to install ipywidgets to run this program with a graphical user interface, please run `pip install ipywidgets` or equivalent in your command line or equivalent (Sorry, end user, you cannot install python packages from within python).") #TODO: this message should be altered to reflect how we're probably just not in a jupyter notebook so any just go to that.
-  elif str(e) == "name 'ipywidgets' is not defined":
-    print('"import ipywidgets" is missing. However, this import should have been imported by the library automatically. If you see this error, please contact the developer so he can update this error message to explain this mysterious circumstance. Maybe putting "import ipywidgets" in your program will help.')
-  elif str(e) == "name 'display' is not defined": # I guess this is just how it looks when invoked from the command line.
-    print("Note: interactive display could not be started (presumably we're on the command line) so you must manually pass hints.") #improve error message
-  else:
-    print("unexpected error occured: ", e)
-    print("Note: interactive display could not be started (presumably we're on the command line) so you must manually pass hints.") #improve error message
+  #we begin with some documentation of error values, for developer use.
+  #str(e)
+  "No module named 'ipywidgets'"  #You need to install ipywidgets to run this program with a graphical user interface, please run `pip install ipywidgets` or equivalent in your command line or equivalent (Sorry, end user, you cannot install python packages from within python).
+  "name 'display' is not defined" # I guess this is just how it looks when invoked from the command line.
+  "name 'ipywidgets' is not defined" #"import ipywidgets" is missing. However, this import should have been imported by the library automatically. If you see this error, please contact the developer so he can update this error message to explain this mysterious circumstance. Maybe putting "import ipywidgets" in your program will help.')
+  print("Note: While trying to display the interactive display, I encountered this error: ", e)
+  print("The interactive display could not be started so you must manually pass hints to the function in python code.") #improve error message
+  print("If you would like to use the interactive display please make sure you are operating in a Jupyter Notebook or similar IPython environment.") #improve error message
+
 
 parser_program = ""
 
@@ -67,7 +63,7 @@ def funravel(data):
     data = [data]
   return inner_parse(data)
 
-def print_table():
+def print_output_table():
   pass
 
 def test_funravel():
