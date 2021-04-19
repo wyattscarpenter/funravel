a744a02a1e179c723b84cac0d698761c0f15dbdd
wyattscarpenter <wyattscarpenter@gmail.com>
2021-04-19 09:18:05 +0100

    more sophisticated table
    also containers

--- funravel.py
+++ funravel.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+#import re
 
 row_hint = ""
 col_hint = ""
@@ -32,36 +33,31 @@ except Exception as e:
   print("The interactive display could not be started so you must manually pass hints to the function in python code.") #improve error message
   print("If you would like to use the interactive display please make sure you are operating in a Jupyter Notebook or similar IPython environment.") #improve error message
 
-#["This is "+ x for x in ['csv', 'json', 'xml', 'xls', 'wikipedia table']]+["Separator "+ x for x in ['⏎⏎', '⏎', 'tab', 'space','|']] + ["Container "+ x for x in ['{}','[]','()', '""', "''"]]
-"""
-hints = [
-['This is csv',
-['This is json',
-['This is xml',
-['This is xls',
-['This is wikimedia table',
-['Separator ⏎⏎',
-['Separator ⏎',
-['Separator tab',
-['Separator space',
-['Separator |',
-['Container {}',
-['Container []',
-['Container ()',
-['Container ""',
-["Container ''",
-]
-"""
 heuristic_table = [ # a table of heuristics, strictly sorted by priority: [test, code, description]
-  #h[0] (eta score) is just some code, which we call with the text, so please don't have side effects.
-  #h[1] (lamdba result) (evaled string!) results in an array of the rows (or, columns). Again, no side-effects please. Tho, python strings should be immutable so risks are low.
-  #TODO: h[2] should be `hints` above. Also maybe it should be h[0] when you get right down to it?
-  [lambda t: t.count("\t")*100 , "text.split('\t')", "tab?"],
-  [lambda t: t.count(",")*200 , "text.split(',')", "comma"], #TODO: bug when , is lower than \t?
-  [lambda t: t.count("\n\n")*2000 , "text.split('\\n\\n')", "⏎⏎"], #NOTE: double escaped \n bc of eval
-  [lambda t: t.count("\n")*1000 , "text.split('\\n')", "⏎"],
-  [lambda t: False, "json.loads(text)", "json"], #could this actually, uh, do anything? #also, it might do everything jsony...
-  [lambda t: 1, "text", "Do Nothing"], #one can always do nothing.
+  #h[0] heuristic (eta score) is just some code, which we call with the text, so please don't have side effects.
+  #h[1] evaled string(!) results in an array of the rows (or, columns). Again, no side-effects please. Tho, python strings should be immutable so risks are low.
+  #h[2] is the identifier of the method, which is also the "hint" and the button label, if those are used.
+  #h[3] is the literal string literal imports the h[1] code will need to work
+
+  [lambda t: t.count("\n\n")*2000, "text.split('\\n\\n')", "Separator ⏎⏎",   "" ], #NOTE: double escaped \n in [1] bc of eval
+  [lambda t: t.count("\n")*1000,   "text.split('\\n')",    "Separator ⏎",    "" ], #NOTE: double escaped \n in [1] bc of eval
+  [lambda t: t.count("\t")*100,    "text.split('\t')",     "Separator tab",  "" ],
+  [lambda t: t.count("|")*50,      "text.split('|')",      "Separator |",    "" ],
+  [lambda t: t.count(" ")*10,      "text.split(' ')",     "Separator space", "" ],
+  
+  [lambda t: sum(map(t.count, ['{','}']))*100 , "re.split('\\{|\\}', text)", "Container {}", "import re"], #might redo this whole concept later
+  [lambda t: sum(map(t.count, ['[',']']))*100 , "re.split('\\[|\\]', text)", "Container []", "import re"],
+  [lambda t: sum(map(t.count, ['(',')']))*100 , "re.split('\\(|\\)', text)", "Container ()", "import re"],
+  [lambda t: sum(map(t.count, ['"','"']))*100 , """re.split('\\"|\\"', text)""", 'Container ""', "import re"],
+  [lambda t: sum(map(t.count, ["'","'"]))*100 , """re.split("\\'|\\'", text)""", "Container ''", "import re"],
+
+  [lambda t: t.count(",")*200 , "text.split(',')", "This is csv", ""], #TODO: bug when , is lower than \t? #todo: rfc-compliance
+  [lambda t: False, "json.loads(text)", "This is json", ""], #could this actually, uh, do anything? #also, it might do everything jsony...
+  [lambda t: False, "json.loads(text)", "This is xml", ""],
+  [lambda t: False, "json.loads(text)", 'This is xls', ""],
+  [lambda t: False, "json.loads(text)", 'This is wikimedia table', ""],
+
+  [lambda t: 1, "[text]", "Do nothing", ""], #you can always do nothing! --laozi (attr) #must be wrapped in list for 2d table purposes
 ]
 
 def funravel(text, row_hint="", col_hint=""):
@@ -71,37 +67,30 @@ def funravel(text, row_hint="", col_hint=""):
   except Exception as e:
     print(e)
     print("Since the text was not a valid filename, I assume it is the text to operate on:") #must improve this message
-  #We have to detect what to parse, compile the operations into the parser_program, and then eval the parser program
-  # We don't just literally do the parse (instead we evaluate), because we want the parser program output to be exactly the same as our output here.
-  # I mean, actually we do do the parse here, but I'm not done writing the program yet...
-  '''
-  #this code seems to work. But we shouldn't use it because we need to use the parser_program instead
+  print("\n"+text)
+
+  #We have to detect what to parse, do one level of parse to detect the second level of parse, put the operations into the parser_program, and then exec the parser program
+  #(We don't just immediately do the parse because we want to ensure the parser program output to be exactly the same as our output here.)
+  #We could use eval here, instead of exec, but I wanted to have multiple lines of code to make the line(s) slightly less "scary". however, this also means the code snippet introduces more variables. Exec seems to be hygenic, but that might be a problem for the end user...
+  
+  parser_program = ""
   m = max(heuristic_table, key = lambda h: h[0](text))
-  #print(m[2])
-  parser_program += m[1]
-  row_separated_text = eval(m[1])
-  text2 = row_separated_text[-1] # you know... this is PROBABLY a typical row.
-  m2 = max(heuristic_table, key = lambda h: h[0](text2))
-  parser_program += m[1] #TODO: need extra step
-  #print(m2[2])
-  table = [eval(m2[1]) for text in row_separated_text] #man... I wonder if this will work.
-  #print_output_table(table)
-  '''
-  #ok this is the actual parser_program version.
-  #we could use eval here, instead of exec, but I wanted to have multiple lines of code to make the line(s) slightly less "scary". however, this also means the code snippet introduces more variables. Exec seems to be hygenic, but that might be a problem for the end user...
-  parser_program = "" #clear this in case it has any junk in it
-  m = max(heuristic_table, key = lambda h: h[0](text))
+  exec(m[3])
+  parser_program += (m[3]+"\n")
   parser_program += ("row_separated_text = "+m[1]+"\n")
+  print(m[1])
   row_separated_text = eval(m[1])
+  print(row_separated_text)
   text2 = row_separated_text[-1] # you know... this is PROBABLY a typical row.
   m2 = max(heuristic_table, key = lambda h: h[0](text2))
+  parser_program += (m2[3]+"\n")
   parser_program += ("table = ["+m2[1]+" for text in row_separated_text] \n")
-  print(text)
   local_variables_from_exec_dict = {"text": text} #this is just how you have to do this
   exec(parser_program, globals(), local_variables_from_exec_dict)
   table = local_variables_from_exec_dict['table']
   print_output_table(table)
   print(parser_program)
+  
   return table
 
 def print_output_table(table, rowsep="<", colsep="|"): #TODO: make non-optional
@@ -115,5 +104,6 @@ def test_funravel():
   print(funravel("f"))
   print(funravel("f,v\tv"))
   print(funravel("we three kings.csv"))
+  print(funravel("its container time.txt"))
 
 test_funravel()
