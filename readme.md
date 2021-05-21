f36d033bedf890f9d9be17e4ace0fe0c9896a190
wyattscarpenter <wyattscarpenter@gmail.com>
2021-05-21 11:09:10 +0100

    misc

--- readme.md
+++ readme.md
@@ -1,2 +1,12 @@
-# funravel
-pick better name later
+# Funravel
+
+Funravel is a python package that facilitates turning structured text into tables (specifically, python dataframes). To use funravel, run the following code:
+
+```
+from funravel import turn_text_into_table
+turn_text_into_table("example.txt") #this can be either a file name or the text itself
+```
+
+If run in a Jupyter Notebook, turn_text_into_table will start an interactive display that allows the user to correct its guesses. Otherwise, it will just guess at the right thing to do.
+
+This project was completed by Wyatt S Carpenter as part of his master's thesis at the University of Cambridge, 2020-2021.
--- test_funravel.py
+++ test_funravel.py
@@ -1,10 +1,10 @@
-from funravel import funravel
+from funravel import turn_text_into_table
 
-funravel("f")
-funravel("f,v\tv")
-funravel("example data/we three kings.csv")
-funravel("example data/its container time.txt")
-funravel("example data/example_metadata.json")
-funravel("example data/wikimedia table example.txt")
-funravel("example data/the sign language a manual of signs 1918 by j schuyler long auxiliary verbs.txt")
-funravel("example data/the sign language a manual of signs 1918 by j schuyler long auxiliary verbs.txt", hint_for_known_format_rule="custom format", hint_for_row_separator_rule="Separator 2 newlines", hint_for_col_separator_rule="Separator —")
+turn_text_into_table("f")
+turn_text_into_table("f,v\tv")
+turn_text_into_table("example data/we three kings.csv")
+turn_text_into_table("example data/its container time.txt")
+turn_text_into_table("example data/example_metadata.json")
+turn_text_into_table("example data/wikimedia table example.txt")
+turn_text_into_table("example data/the sign language a manual of signs 1918 by j schuyler long auxiliary verbs.txt")
+turn_text_into_table("example data/the sign language a manual of signs 1918 by j schuyler long auxiliary verbs.txt", hint_for_known_format_rule="custom format", hint_for_row_separator_rule="Separator 2 newlines", hint_for_col_separator_rule="Separator —")
